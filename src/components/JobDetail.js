a3d51d2ded2e2e1757fdb5b161166415f5fb9375
Emmilie Estabillo <emsestabillo@gmail.com>
2021-02-22 14:40:56 -0500

    Add taglist to jobdetail component

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -9,14 +9,21 @@ function JobDetail(){
           const {id, company, logo, featured, position, postedAt, location, contract, languages, role, level, tools} = job
       return (
         <article key={id}>
-        <img src={logo} alt="company logo"/>
+          <img src={logo} alt="company logo"/>
           <p>{company} </p>
-      <h2>Position:{position}</h2>
-      {featured ? <p>feat</p> : '' }
-      {job.new ? <p>new!</p> : '' }
-     <p>{postedAt}</p>
-     <p>{contract}</p>
-     <p>{location}</p>
+          <h2>{position}</h2>
+          {featured ? <p>featured</p> : '' }
+          {job.new ? <p>new!</p> : '' }
+          <p>{postedAt}</p>
+          <p>{contract}</p>
+          <p>{location}</p>
+
+          <ul className="tags">
+            {languages.map((language) => <li key={id + language}>{language}</li>)}
+            <li>{role}</li>
+            <li>{level}</li>
+            {tools.map((tool) => <li key={id + tool}>{tool}</li>)}        
+          </ul>
     
      
         </article>
