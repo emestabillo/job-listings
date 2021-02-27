353f6b69a69589bca0750fa268c1fef803323f87
emestabillo <emsestabillo@gmail.com>
2021-02-26 21:31:30 -0500

    Edit job details to show alt text and button values

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -1,38 +1,49 @@
-import React from 'react'
-import {openings} from '../data.js'
+import React from "react";
+import { openings } from "../data.js";
 
-function JobDetail(){
+function JobDetail() {
   return (
     <>
       {openings.map((job) => {
-          const {id, company, logo, featured, position, postedAt, location, contract, languages, role, level, tools} = job
+        const {
+          id,
+          company,
+          logo,
+          featured,
+          position,
+          postedAt,
+          location,
+          contract,
+          languages,
+          role,
+          level,
+          tools,
+        } = job;
 
-          const tooling = [...languages, role, level, ...tools].flat()
-   
-      return (
-        <article key={id} style={{marginBottom: "3rem"}}>
-          <img src={logo} alt="company logo"/>
-          <p>{company} </p>
-          <h2>{position}</h2>
-          {featured && <p>featured</p>}
-          {job.new && <p>new!</p>}
-          <p>{postedAt}</p>
-          <p>{contract}</p>
-          <p>{location}</p>
-          <ul>{tooling.map((tool, index) => 
-            <li key={index}>
-              <button>
-                {tool}
-              </button>
-              
-            </li>)}
-          </ul>
-        </article>
-        )}
-      )}              
+        const tooling = [...languages, role, level, ...tools].flat();
+
+        return (
+          <article key={id} style={{ marginBottom: "3rem" }}>
+            <img src={logo} alt={`{company} logo`} />
+            <p>{company} </p>
+            <h2>{position}</h2>
+            {featured && <p>featured</p>}
+            {job.new && <p>new!</p>}
+            <p>{postedAt}</p>
+            <p>{contract}</p>
+            <p>{location}</p>
+            <ul style={{ listStyle: "none", padding: "0" }}>
+              {tooling.map((tool, index) => (
+                <li key={index}>
+                  <button value={tool}>{tool}</button>
+                </li>
+              ))}
+            </ul>
+          </article>
+        );
+      })}
     </>
-    
-  )
+  );
 }
 
-export default JobDetail
+export default JobDetail;
